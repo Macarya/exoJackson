fb8c20c8223caa291711e35ac6c6653d2d1ca7ea
Pragonix <pragonix@laposte.net>
2018-11-30 14:45:38 +0100

    classe adresse

--- src/main/java/ExoMaven/Address.java
+++ src/main/java/ExoMaven/Address.java
@@ -6,6 +6,29 @@ public class Address {
 
 	public Address() {
 		// TODO Auto-generated constructor stub
+		setRue("de la Nouvelle france");
+		setNum(140) ;
+	}
+	
+	public Address(String rue, Integer num) {
+		this.setRue(rue) ;
+		this.setNum(num) ;
+	}
+
+	public Integer getNum() {
+		return num;
+	}
+
+	public void setNum(Integer num) {
+		this.num = num;
+	}
+
+	public String getRue() {
+		return rue;
+	}
+
+	public void setRue(String rue) {
+		this.rue = rue;
 	}
 
 }
