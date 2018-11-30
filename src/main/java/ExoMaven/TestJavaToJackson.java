7c48802d8c0afcae7c424f3efcf80b9173b1fc75
Yannis Gaziello <yannis@gaziello.eu>
2018-11-30 14:27:57 +0100

    create structure

---
+++ src/main/java/ExoMaven/TestJavaToJackson.java
@@ -0,0 +1,7 @@
+package ExoMaven;
+
+import junit.framework.TestCase;
+
+public class TestJavaToJackson extends TestCase {
+
+}
---
+++ src/main/java/ExoMaven/testJacksonToJava.java
@@ -0,0 +1,8 @@
+package ExoMaven;
+
+import junit.framework.TestCase;
+
+public class testJacksonToJava extends TestCase {
+
+	
+}
