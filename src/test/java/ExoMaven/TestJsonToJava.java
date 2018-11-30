ae768f7ef595ab28268fa8f372565b3d7db45975
Nicolas BARNIER <nbarnier@a101-17.iut.univ-paris8.fr>
2018-11-30 15:04:49 +0100

    ajout testJsonTojava

--- src/test/java/ExoMaven/TestJsonToJava.java
+++ src/test/java/ExoMaven/TestJsonToJava.java
@@ -2,13 +2,31 @@ package ExoMaven;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Test;
+
+import com.example.exoMaven1.ExoMaven1ApplicationTests.Person;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TestJsonToJava {
 
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void jsonToJava() {
+		String json = "{\"name\":\"moi\", \"address\":{}}";
+		ObjectMapper om = new ObjectMapper();
+		try {
+			Personne p = om.readValue(json, Personne.class);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//Assert.assertEquals(expected, actual);
 	}
 
 }
