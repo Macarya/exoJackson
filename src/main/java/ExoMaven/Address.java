a93a0221cdaebe710f8cdfe62eb4c376efde6e5f
Yannis Gaziello <yannis@gaziello.eu>
2018-11-30 14:18:01 +0100

    maven project

---
+++ src/main/java/ExoMaven/Address.java
@@ -0,0 +1,11 @@
+package ExoMaven;
+
+public class Address {
+	private String rue;
+	private Integer num;
+
+	public Address() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/ExoMaven/Personne.java
@@ -0,0 +1,19 @@
+package ExoMaven;
+
+public class Personne {
+	private Address adr;
+	private String name;
+	private Integer age;
+	
+
+	public Personne() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Personne(Address adr, String name, Integer age) {
+		this.adr=adr;
+		this.age=age;
+		this.name=name;
+	}
+
+}
