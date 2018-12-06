ab49152784e3d3c614e46ecc753a45dd7163587f
ROMAIN LENOIR <rlenoir@a101-02.iut.univ-paris8.fr>
2018-12-06 11:24:19 +0100

    Fin de l'exercice

--- src/main/java/ExoMaven/Personne.java
+++ src/main/java/ExoMaven/Personne.java
@@ -9,6 +9,43 @@ public class Personne {
 	public Personne() {
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((adr == null) ? 0 : adr.hashCode());
+		result = prime * result + ((age == null) ? 0 : age.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Personne other = (Personne) obj;
+		if (adr == null) {
+			if (other.adr != null)
+				return false;
+		} else if (!adr.equals(other.adr))
+			return false;
+		if (age == null) {
+			if (other.age != null)
+				return false;
+		} else if (!age.equals(other.age))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 	public Personne(Address adr, String name, Integer age) {
 		this.adr=adr;
 		this.age=age;
--- src/test/java/ExoMaven/TestJsonToJava.java
+++ src/test/java/ExoMaven/TestJsonToJava.java
@@ -1,26 +1,62 @@
 package ExoMaven;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import java.io.IOException;
 
 import org.junit.Test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TestJsonToJava {
 	
 	@Test
-	public void jsonToJava() {
-		String json = "{\"name\":\"moi\", \"adr\":{}}";
+	public void testAddress() {
+		String json1 = "{\"rue\":\"rue de la Nouvelle france\",\"num\":140}";
+		String json2 = "{\"rue\":\"avenue de paris\",\"num\":12}";
+		String jsonFalse1 = "{\"rue\":\"de la Nouvelle france\",\"num\":140}";
+		String jsonFalse2 = "{\"rue\":\"rue de la Nouvelle france\",\"num\":139}";
+		Address adr1 = new Address("rue de la Nouvelle france", 140);
+		Address adr2 = new Address("avenue de paris", 12);
 		ObjectMapper om = new ObjectMapper();
 		try {
-			Personne p = om.readValue(json, Personne.class);
+			assertEquals(adr1, om.readValue(json1, Address.class));
+			assertNotEquals(adr1, om.readValue(jsonFalse1, Address.class));
+			assertNotEquals(adr1, om.readValue(jsonFalse2, Address.class));
+			assertEquals(adr2, om.readValue(json2, Address.class));
+			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//Assert.assertEquals(expected, actual);
 	}
+	
+	
+	@Test
+	public void testPersonne() {
+		String json1 = "{\"adr\":{\"rue\":\"rue de la Nouvelle france\",\"num\":140},\"name\":\"Napoléon\",\"age\":0}";
+		String jsonFalse1 = "{\"adr\":{\"rue\":\"rue de la Nouvelle france\",\"num\":140},\"name\":\"Napléon\",\"age\":0}";
+		String jsonFalse2 = "{\"adr\":{\"rue\":\"rue de la Nuvelle france\",\"num\":140},\"name\":\"Napoléon\",\"age\":0}";
+		String jsonFalse3 = "{\"adr\":{\"rue\":\"rue de la Nuvelle france\",\"num\":140},\"name\":\"Napoléon\",\"age\":1}";
+		String json2 = "{\"adr\":{\"rue\":\"avenue de paris\",\"num\":12},\"name\":\"Gertrude\",\"age\":12}";
+		Address adr1 = new Address("rue de la Nouvelle france", 140);
+		Address adr2 = new Address("avenue de paris", 12);
+		Personne pers1 = new Personne(adr1,"Napoléon", 0);
+		Personne pers2 = new Personne(adr2,"Gertrude", 12);
+		ObjectMapper om = new ObjectMapper();
+
+		try {
+			assertEquals(pers1, om.readValue(json1, Personne.class));
+			assertEquals(pers2, om.readValue(json2, Personne.class));
+			assertNotEquals(pers1, om.readValue(jsonFalse1, Personne.class));
+			assertNotEquals(pers1, om.readValue(jsonFalse2, Personne.class));
+			assertNotEquals(pers1, om.readValue(jsonFalse3, Personne.class));
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 
 }
